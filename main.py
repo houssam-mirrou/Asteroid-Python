f2990f28aadfdb85a71b8a15533fd4a7479883c9
houssam-mirrou <hossam.mirrou@gmail.com>
2026-05-03 18:48:40 +0100

    I made the a little triangle in the middle of the screen as the player

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import pygame
-from constants import SCREEN_HEIGHT,SCREEN_WIDTH
+from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
+from Player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True :
         log_state()
+        amount_time = clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-            amount_time = clock.tick(60)
-            dt = amount_time/1000
-            print(dt)
-            
+                return    
         screen.fill("black")
+        dt = amount_time/1000
+        player.draw(screen)
+        
         pygame.display.flip()
     
     
