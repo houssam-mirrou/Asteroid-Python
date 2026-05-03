e028da9e0ad2051d5e340211d6920a1d4b8a6e92
houssam-mirrou <hossam.mirrou@gmail.com>
2026-05-03 19:08:06 +0100

    I grouped the drawables to be simple to use

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from Player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -13,13 +16,13 @@ def main():
         log_state()
         amount_time = clock.tick(60)
         dt = amount_time/1000
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            player.update(dt)
+            updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for dr in drawable:
+            dr.draw(screen)
         
         pygame.display.flip()
     
