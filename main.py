03f83a332846190cf281327f551933255ec84666
houssam-mirrou <hossam.mirrou@gmail.com>
2026-05-03 18:34:43 +0100

    I made the game take less resources from the cpu and created a new class called circleshape

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True :
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            amount_time = clock.tick(60)
+            dt = amount_time/1000
+            print(dt)
+            
         screen.fill("black")
         pygame.display.flip()
     
-    
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
 
 
